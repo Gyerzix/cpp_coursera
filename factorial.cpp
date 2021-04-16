2cfe4a184ab8dcfe49eed77bcdc99e75f19d089e
Shakhin <shakhingadzhiev@yandex.ru>
2021-04-16 20:25:22 +0300

    commit

---
+++ factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int Factorial(int i) {
+	int p=1;
+	if (i>=0) {
+		for (int s=1; s<=i; ++s) {
+			p=p*s;
+		}
+	return p;
+	}
+	else 
+		return 1;
+}
