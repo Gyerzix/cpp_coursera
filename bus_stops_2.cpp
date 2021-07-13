1e580ee16da4f0174127570c2192e9fb4b1aabb3
Shakhin <67441571+Gyerzix@users.noreply.github.com>
2021-07-13 12:55:59 +0300

    Add files via upload

---
+++ bus_stops_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int m = 1; // номер автобуса
+	map<int, vector<string>> bus_stops;
+	int Q; cin >> Q;
+	for (int i = 0; i < Q; i++) {
+		int c = 0; // 1 - если есть такой маршрут, иначе - 0
+		int j; // номер существующего автобуса
+		int count_stops; cin >> count_stops;
+		vector<string> v(count_stops);
+		for (string& s : v) {
+			cin >> s;
+		}
+
+		for (const auto& item : bus_stops) {
+			if (item.second == v) {
+				c = 1;
+				j = item.first;
+			}
+		}
+		if (c == 1) {
+			cout << "Already exists for " << j << endl;
+		}
+		else if (c == 0) {
+			bus_stops[m] = v;
+			cout << "New bus " << m << endl;
+			m += 1;
+		}
+	}
+	return 0;
+}
