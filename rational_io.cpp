fbaa71507424747a67c165f52928f88469275aee
Shakhin <67441571+Gyerzix@users.noreply.github.com>
2021-08-07 00:04:25 +0300

    Add files via upload

---
+++ rational_io.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <iomanip>
+
+using namespace std;
+
+class Rational {
+public:
+    Rational() { // Реализуйте конструктор по умолчанию
+        num = 0;
+        den = 1;
+    }
+
+    Rational(int numerator, int denominator) { // Реализуйте конструктор
+        if (numerator > 0 && denominator < 0) {
+            int k = gcd(abs(numerator), abs(denominator));
+            if (gcd(abs(numerator), abs(denominator))) {
+                num = -numerator / k;
+                den = -denominator / k;
+            }
+            else {
+                num = -numerator;
+                den = -denominator;
+            }
+
+        }
+        else if (numerator < 0 && denominator < 0) {
+            int k = gcd(abs(numerator), abs(denominator));
+            if (gcd(abs(numerator), abs(denominator))) {
+                num = -numerator / k;
+                den = -denominator / k;
+            }
+            else {
+                num = -numerator;
+                den = -denominator;
+            }
+        }
+        else if (numerator == 0) {
+            num = 0;
+            den = 1;
+        }
+        else {
+            int k = gcd(abs(numerator), abs(denominator));
+            if (gcd(abs(numerator), abs(denominator))) {
+                num = numerator / k;
+                den = denominator / k;
+            }
+            else {
+                num = numerator;
+                den = denominator;
+            }
+        }
+    }
+
+    int Numerator() const { // Реализуйте этот метод
+        return num;
+    }
+
+    int Denominator() const { // Реализуйте этот метод
+        return den;
+    }
+
+private:
+    int num;
+    int den;
+};
+
+// Вставьте сюда реализацию operator == для класса Rational из второй части
+bool operator==(const Rational& lhs, const Rational& rhs) {
+    if (lhs.Numerator() == rhs.Numerator() && lhs.Denominator() == rhs.Denominator()) {
+        return true;
+    }
+    return false;
+}
+
+// Реализуйте для класса Rational операторы << и >>
+ostream& operator<<(ostream& stream, const Rational& v) {
+    stream << setfill('0');
+    stream << v.Numerator() << '/' << v.Denominator();
+    return stream;
+}
+
+istream& operator>>(istream& stream, Rational& v) {
+    int n, d;
+    char op;
+    if (stream) {
+        stream >> n >> op >> d;
+        if (op == '/' && stream) {
+            v = Rational(n, d);
+        }
+    }
+    return stream;
+}
+
+
+int main() {
+    {
+        ostringstream output;
+        output << Rational(-6, 8);
+        if (output.str() != "-3/4") {
+            cout << "Rational(-6, 8) should be written as \"-3/4\"" << endl;
+            return 1;
+        }
+    }
+
+    {
+        istringstream input("5/7");
+        Rational r;
+        input >> r;
+        bool equal = r == Rational(5, 7);
+        if (!equal) {
+            cout << "5/7 is incorrectly read as " << r << endl;
+            return 2;
+        }
+    }
+
+    {
+        istringstream input("");
+        Rational r;
+        bool correct = !(input >> r);
+        if (!correct) {
+            cout << "Read from empty stream works incorrectly" << endl;
+            return 3;
+        }
+    }
+
+    {
+        istringstream input("5/7 10/8");
+        Rational r1, r2;
+        input >> r1 >> r2;
+        bool correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+        if (!correct) {
+            cout << "Multiple values are read incorrectly: " << r1 << " " << r2 << endl;
+            return 4;
+        }
+
+        input >> r1;
+        input >> r2;
+        correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+        if (!correct) {
+            cout << "Read from empty stream shouldn't change arguments: " << r1 << " " << r2 << endl;
+            return 5;
+        }
+    }
+
+    {
+        istringstream input1("1*2"), input2("1/"), input3("/4");
+        Rational r1, r2, r3;
+        input1 >> r1;
+        input2 >> r2;
+        input3 >> r3;
+        bool correct = r1 == Rational() && r2 == Rational() && r3 == Rational();
+        if (!correct) {
+            cout << "Reading of incorrectly formatted rationals shouldn't change arguments: "
+                 << r1 << " " << r2 << " " << r3 << endl;
+
+            return 6;
+        }
+    }
+
+    cout << "OK" << endl;
+    return 0;
+}
